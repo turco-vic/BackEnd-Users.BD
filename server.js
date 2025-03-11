e0e83988bf4a5c0d6032f8e195a778a8df961067
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-03-11 09:15:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/users", userRoutes);
+app.use("/api", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
